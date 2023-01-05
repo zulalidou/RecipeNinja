f9a7adcba6cdf4588a006757805f431781656b74
Zul <zulkifl.alidou@gmail.com>
2023-01-04 16:46:37 -0800

    Modify build script

---
+++ client/build/precache-manifest.c4519f62468692bd7a22a182f2e0f5a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00d07691cc8d821b22df7eee242a1c2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ece7ea4e964516a90a4a",
+    "url": "/static/css/main.e47fcc19.chunk.css"
+  },
+  {
+    "revision": "19da6244feb0d0df83c0",
+    "url": "/static/js/2.e39a908b.chunk.js"
+  },
+  {
+    "revision": "24131b4bf7413192deedb64735390eae",
+    "url": "/static/js/2.e39a908b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ece7ea4e964516a90a4a",
+    "url": "/static/js/main.83239baa.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
